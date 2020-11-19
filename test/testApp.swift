1ed721c8a43003512a0bd6d86f6730a839aa4d38
Alexia Colmenero <acolmenero@berkeley.edu>
2020-11-19 14:34:42 -0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Alex Colmenero on 11/19/20.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
